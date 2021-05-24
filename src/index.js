08dfb364aa1e605a3c2a8d391cb4ff3bcacc1a24
Daniel Schmid <daniel.schmid@libertymail.net>
2021-05-24 14:58:15 +0200

    feat: add voting component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-import ChoiceBar from "./components/ChoiceBar";
+import VotingComponent from "./components/VotingComponent";
 import "./index.css";
 
+const vote = {
+  title: "How is your day?",
+  description: "Tell me: how has your day been so far?",
+  choices: [
+    { id: "choice_1", title: "Good", count: 7 },
+    { id: "choice_2", title: "Bad", count: 12 },
+    { id: "choice_3", title: "Not sure yet", count: 1 },
+  ],
+};
+
 ReactDom.render(
-  <ChoiceBar title="JavaScript" percent={37} />,
+  <VotingComponent vote={vote} />,
   document.getElementById("root")
 );
